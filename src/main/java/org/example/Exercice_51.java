597270e613819d456366c26d20402618978c6a5e
unknown <vincentpolska@gmail.com>
2025-05-12 14:51:44 +0200

    exercice_51

---
+++ src/main/java/org/example/Exercice_51.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Exercice_51 {
+    public static void main(String[] args) {
+        /*
+        1.Créer une variable nom et une variable prenom
+        2.Afficher la phrase suivante "Bonjour {prenom} {NOM}."
+        3.Remplacer les valeurs entre chevrons par les variables crééesprécédemment
+         */
+
+        String nom, prenom;
+        prenom = "James";
+        nom = "Bond";
+        System.out.println("Bonjour " + prenom + " " + nom);
+    }
+}
